f2e4f1ef256c700cd1337737f99ec6cc2ec8f9ff
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2017-05-18 19:51:15 +0200

    Add timestamp conversion

--- src/main/java/be/mira/jongeren/mailinglist/util/date/LocalDateTimeAttributeConverter.java
+++ src/main/java/be/mira/jongeren/mailinglist/util/date/LocalDateTimeAttributeConverter.java
@@ -1,4 +1,21 @@
 package be.mira.jongeren.mailinglist.util.date;
 
-public class LocalDateTimeAttributeConverter {
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Converter(autoApply = true)
+public class LocalDateTimeAttributeConverter implements AttributeConverter<LocalDateTime, Timestamp> {
+
+
+    @Override
+    public Timestamp convertToDatabaseColumn(LocalDateTime localDateTime) {
+        return (localDateTime == null ? null : Timestamp.valueOf(localDateTime));
+    }
+
+    @Override
+    public LocalDateTime convertToEntityAttribute(Timestamp timestamp) {
+        return (timestamp == null ? null : timestamp.toLocalDateTime());
+    }
 }
