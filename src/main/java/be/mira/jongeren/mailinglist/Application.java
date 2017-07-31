a6ea06665e3b159332d25b3b8d0ac4a7d0a266ea
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2017-07-31 21:34:39 +0200

    Add some souts for debugging

--- src/main/java/be/mira/jongeren/mailinglist/Application.java
+++ src/main/java/be/mira/jongeren/mailinglist/Application.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(System.getProperty("JDBC_DATABASE_URL"));
         SpringApplication.run(Application.class, args);
     }
 
