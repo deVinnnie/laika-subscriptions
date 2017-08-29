d0c17b6883c2f80a601470d68f243dbac87bbbd9
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2017-08-29 18:46:56 +0200

    Add custom error pages

---
+++ src/main/java/be/mira/jongeren/mailinglist/controllers/ErrorController.java
@@ -0,0 +1,54 @@
+package be.mira.jongeren.mailinglist.controllers;
+
+import org.springframework.boot.autoconfigure.web.ErrorViewResolver;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@Controller
+public class ErrorController implements ErrorViewResolver {
+
+    @RequestMapping(value = "/error", method = RequestMethod.GET)
+    public ModelAndView renderErrorPage(HttpServletRequest httpRequest) {
+
+        ModelAndView errorPage = new ModelAndView("errors/index");
+        String errorMsg = "";
+        int httpErrorCode = getErrorCode(httpRequest);
+
+        switch (httpErrorCode) {
+            case 400: {
+                errorMsg = "Http Error Code: 400. Bad Request";
+                break;
+            }
+            case 401: {
+                errorMsg = "Http Error Code: 401. Unauthorized";
+                break;
+            }
+            case 404: {
+                errorMsg = "Http Error Code: 404. Resource not found";
+                break;
+            }
+            case 500: {
+                errorMsg = "Http Error Code: 500. Internal Server Error";
+                break;
+            }
+        }
+        errorPage.addObject("errorMsg", errorMsg);
+        return errorPage;
+    }
+
+    private int getErrorCode(HttpServletRequest httpRequest) {
+        return (Integer) httpRequest
+                .getAttribute("javax.servlet.error.status_code");
+    }
+
+    @Override
+    public ModelAndView resolveErrorView(HttpServletRequest request, HttpStatus status, Map<String, Object> model) {
+        return renderErrorPage(request);
+    }
+}
