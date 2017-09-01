847fbaf11f40937bad100974e548985d47a43ccb
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2017-09-01 20:06:04 +0200

    Add subscription dates for dummies

--- src/main/resources/002-dummy-subscribers.sql
+++ src/main/resources/002-dummy-subscribers.sql
@@ -1,7 +1,7 @@
-INSERT INTO subscriber(id, voornaam, achternaam, email, active)
-VALUES('10', 'Luke', 'Skywalker', 'luke@rebellion.org', true),
-      ('20', 'Leia', 'Organa', 'leia@rebellion.org', true),
-      ('30', 'Han', 'Solo', 'han@rebellion.org', true);
+INSERT INTO subscriber(id, voornaam, achternaam, email, active, subscription_date)
+VALUES('10', 'Luke', 'Skywalker', 'luke@rebellion.org', true, '2015-01-01 18:00:00'),
+      ('20', 'Leia', 'Organa', 'leia@rebellion.org', true, '2015-01-01 18:00:00'),
+      ('30', 'Han', 'Solo', 'han@rebellion.org', true, '2015-01-01 18:00:00');
 
 INSERT INTO SUBSCRIPTION_LIST_SUBSCRIBERS (subscriptions_id, subscribers_id)
 VALUES('10', '10'),
