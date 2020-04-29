41f90bc35503d5d4b7dc07315e7999d45b2ba961
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2020-04-29 18:51:23 +0200

    Remove unused import

--- src/main/java/be/mira/jongeren/mailinglist/repository/SubscriptionListRepository.java
+++ src/main/java/be/mira/jongeren/mailinglist/repository/SubscriptionListRepository.java
@@ -4,8 +4,6 @@ import be.mira.jongeren.mailinglist.domain.SubscriptionList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface SubscriptionListRepository extends JpaRepository<SubscriptionList, Long> {
 
