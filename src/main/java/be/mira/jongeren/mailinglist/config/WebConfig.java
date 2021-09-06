17704cc7c4e2b3d7ac61e6b1d077805d10562571
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2021-09-06 17:09:41 +0200

    Bump versions (Spring, Gradle)

--- src/main/java/be/mira/jongeren/mailinglist/config/WebConfig.java
+++ src/main/java/be/mira/jongeren/mailinglist/config/WebConfig.java
@@ -1,6 +1,8 @@
 package be.mira.jongeren.mailinglist.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +12,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("auth/login").setViewName("auth/login");
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer
+            .defaultContentType(MediaType.TEXT_HTML)
+            .ignoreAcceptHeader(true);
     }
 }
